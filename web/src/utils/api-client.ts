26d14c0878da090c568242c65f74657b4416f17c
Alejandro Monasterios <alemonas@gmail.com>
2021-12-14 15:33:59 -0400

    mapped influencers data

--- web/src/utils/api-client.ts
+++ web/src/utils/api-client.ts
@@ -1,7 +1,38 @@
-import influencersData from '../../assets/influencerData_500.json'
+import influencersDataSample from 'assets/influencerData_500.json'
+import influencersDataAll from 'assets/influencerData_bigger.json'
 
-console.log({influencersData})
+function mapData(data: any) {
+  return data.map((influencerData: any) => {
+    const {
+      firstName,
+      lastName,
+      gender,
+      tags,
+      previousPaidExperience,
+      skillSet,
+    } = influencerData
+    const reducer = (previousValue: any, currentValue: any) => {
+      return previousValue + currentValue.yearsOfExperience
+    }
 
-export function getItems() {
-  return influencersData
+    const yearsOfExperience = skillSet ? skillSet.reduce(reducer, 0) : 0
+
+    return {
+      firstName,
+      lastName,
+      gender,
+      tags,
+      previousPaidExperience,
+      skillSet,
+      yearsOfExperience,
+    }
+  })
+}
+
+export function getSampleItems() {
+  return mapData(influencersDataSample)
+}
+
+export function getAllItems() {
+  return mapData(influencersDataAll)
 }
