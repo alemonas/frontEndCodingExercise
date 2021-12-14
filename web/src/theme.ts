44823d37b37304612c68b7aba7720517247b3ac8
Alejandro Monasterios <alemonas@gmail.com>
2021-12-13 22:47:22 -0400

    create basic theme file

---
+++ web/src/theme.ts
@@ -0,0 +1,11 @@
+import {Theme} from '@theme-ui/core' 
+import {swiss} from "@theme-ui/presets"
+
+const theme: Theme = {
+    ...swiss,
+    styles: {
+        ...swiss.styles
+    }
+}
+
+export default theme
