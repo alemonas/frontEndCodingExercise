79cf2ebea7c634b97c405d24d5708f0b7fadc9c4
Alejandro Monasterios <alemonas@gmail.com>
2021-12-14 14:51:41 -0400

    add basic text startwith filter to each column

--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -1,12 +1,41 @@
-import {useTable} from 'react-table'
+import React from 'react'
+import {useFilters, useGlobalFilter, useTable} from 'react-table'
 import {Container} from 'theme-ui'
 
+function DefaultColumnFilter({
+  column: {filterValue, preFilteredRows, setFilter},
+}: any) {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined)
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
+}
+
 function Table({columns, data}: any) {
+  const defaultColumn: any = React.useMemo(
+    () => ({
+      Filter: DefaultColumnFilter,
+    }),
+    [],
+  )
+
   const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} =
-    useTable({
-      columns,
-      data,
-    })
+    useTable(
+      {
+        columns,
+        data,
+        defaultColumn,
+      },
+      useFilters,
+      useGlobalFilter,
+    )
 
   return (
     <Container bg="muted">
@@ -15,7 +44,10 @@ function Table({columns, data}: any) {
           {headerGroups.map((headerGroup: any) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column: any) => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                <th {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                  <div>{column.canFilter ? column.render('Filter') : null}</div>
+                </th>
               ))}
             </tr>
           ))}
