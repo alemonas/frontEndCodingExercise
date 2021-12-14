a8924c172c0ce05345a6c1df99057f7811ba5722
Alejandro Monasterios <alemonas@gmail.com>
2021-12-14 14:50:31 -0400

    test theme variants

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -1,11 +1,19 @@
-import {swiss} from "@theme-ui/presets"
-import {Theme} from 'theme-ui' 
+import {swiss} from '@theme-ui/presets'
+import {Theme} from 'theme-ui'
 
 const theme: Theme = {
-    ...swiss,
-    styles: {
-        ...swiss.styles
-    }
+  ...swiss,
+  styles: {
+    ...swiss.styles,
+  },
+  text: {
+    ...swiss.text,
+    table: {
+      previousPaidExperience: {
+        padding: 1,
+      },
+    },
+  },
 }
 
 export default theme
